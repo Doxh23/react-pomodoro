9b098b179a75bdfbfe15b53a1cfdafc6dcd0947a
doxh23 <adrien.peters95@gmail.com>
2021-10-19 14:45:38 +0200

    netifly

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,4 @@
+
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 13,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "react-hooks"
-    ],
-    "rules": {
-        "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
-        "react-hooks/exhaustive-deps": "warn" // Checks effect dependencies
-      }
-    }
-;
+    extends: "@becode",
+};
